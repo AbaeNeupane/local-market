628fb2852aa44e1d0a44a6525d1527873417807d
Abae Neupane <108645361+AbaeNeuapne@users.noreply.github.com>
2026-03-04 15:23:25 -0500

    New market

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,28 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from . import views
-from dashboard.admin_site import local_market_admin
-
-# Register all app admins with our custom site
-from users.admin import ProfileAdmin
-from users.models import Profile
-from products.admin import ProductAdmin, CategoryAdmin
-from products.models import Product, Category, ProductImage
-from orders.admin import OrderAdmin
-from orders.models import Order, OrderStatusHistory
-from payments.admin import PaymentAdmin
-from payments.models import Payment
-
-local_market_admin.register(Profile, ProfileAdmin)
-local_market_admin.register(Product, ProductAdmin)
-local_market_admin.register(Category, CategoryAdmin)
-local_market_admin.register(Order, OrderAdmin)
-local_market_admin.register(Payment, PaymentAdmin)
 
 urlpatterns = [
-    path('admin/', local_market_admin.urls),
-    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
